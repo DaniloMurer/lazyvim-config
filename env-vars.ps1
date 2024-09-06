801b50c087c62b8ec12b4434f30452b9e6a6192e
Danilo Jakob <danilojakob@protonmail.com>
2024-09-06 22:00:18 +0200

    feat: removed supermaven llm plugin because real programmers read documentation

--- env-vars.ps1
+++ env-vars.ps1
@@ -1,3 +1,2 @@
-// powershell 7 path
 [System.Environment]::SetEnvironmentVariable("NVIM_SHELL", "C:\Program Files\PowerShell\7\pwsh.exe", [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable("CODESTATS_API_KEY", "<YOUR_API_KEY>", [System.EnvironmentVariableTarget]::User)
